5011104d179b8b08b39e96e1b971d67db8a5eb66
DimaShabatyn <112180483+DimaShabatyn@users.noreply.github.com>
2022-11-13 12:37:12 +0200

    Кнопка меню

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,14 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-modal-button]");
+  
+menuBtnRef.addEventListener("click", () => {
+    
+  menuBtnRef.classList.toggle("is-open");
+
+  });
+})();
